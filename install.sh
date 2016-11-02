3916ad78f19e5998a415881f5e127fba2530bf1e
Yehonal <yehonal.azeroth@gmail.com>
2016-11-02 13:28:56 +0100

    update

--- install.sh
+++ install.sh
@@ -13,8 +13,6 @@ mod_path="hw-core/js-modules/"
 # ADD DEPENDENCIES
 #
 
-Joiner:add_file "http://www.java2s.com/Code/JarDownload/javax.servlet/javax.servlet-3.0.jar.zip"      "$mod_path/javax/servlet/javax.servlet-3.0.jar.zip" && unzip "$mod_path/javax/servlet/javax.servlet-3.0.jar.zip"
+Joiner:add_file "http://www.java2s.com/Code/JarDownload/javax.servlet/javax.servlet-3.0.jar.zip"      "$mod_path/javax/servlet/javax.servlet-3.0.jar.zip" --unzip
 
-if Joiner:with_dev ; then
-    # nothing to do for now
-fi
+
